875c9c00cfaf7b3d632ffa13f1d3f7309685a066
Theresa Thoraldson <theresa.thoraldson@outlook.com>
2016-07-24 15:14:27 -0500

    added a testing js file

---
+++ ajaxAndJqueryTest.js
@@ -0,0 +1,19 @@
+/*
+_____                           _ _    __   __                        _  __
+/  __ \                         (_) |   \ \ / /                       | |/ _|
+| /  \/ ___  _ __ ___  _ __ ___  _| |_   \ V /___  _   _ _ __ ___  ___| | |_
+| |    / _ \| '_ ` _ \| '_ ` _ \| | __|   \ // _ \| | | | '__/ __|/ _ \ |  _|
+| \__/\ (_) | | | | | | | | | | | | |_    | | (_) | |_| | |  \__ \  __/ | |
+\____/\___/|_| |_| |_|_| |_| |_|_|\__|   \_/\___/ \__,_|_|  |___/\___|_|_|
+(c) tthoraldson 2016 | https://github.com/tthoraldson/commit_yourself
+                              v. 0.0.1
+*/
+
+
+gitUName = "tthoraldson" // temporary, just for testing purposes.
+
+// date var the same as github's data-date
+var d = new Date();
+var dataDate = (d.getMonth() + 1) + "-" + d.getDate() + "-" + (d.getFullYear() - 2000);
+
+console.log(dataDate);
